75a818d4324786c995b8cf6136524e269c27029f
Marta <martavivesluis@gmail.com>
2017-11-15 17:55:34 +0100

    jejej

--- marta/src/main/java/ProducManagerImpl.java
+++ marta/src/main/java/ProducManagerImpl.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class ProducManagerImpl implements ProductManager {
 
@@ -6,12 +9,12 @@ public class ProducManagerImpl implements ProductManager {
     HashMap<Integer,Pedido> todosLosPedidosEver; // per recuperar pedidos i afegir-hi productes
     HashMap<String, Usuario> usuarios; // per recuperar usuaios a partir del nom
     Listapedidos listaDePedidos; // cua de pedidos
-                                   //no cal hash NomUsario->Array de pedidos pq Usuari tindra attribut pedidos
+    //no cal hash NomUsario->Array de pedidos pq Usuari tindra attribut pedidos
     HashMap<String,Producto> productosenventa;//productos disponibles
     int idsdepedido = 0;
 
+    private static final Logger logger = LogManager.getLogger(ProductManager.class.getName());
     private static ProducManagerImpl mimundo;
-
     private ProducManagerImpl() {
         this.usuarios = new HashMap<String, Usuario>();
         this.productosVendidos = new HashMap<String, Integer>();
@@ -24,11 +27,28 @@ public class ProducManagerImpl implements ProductManager {
             mimundo = new ProducManagerImpl();
         return mimundo;
     }
-public void añadirUsuario(Usuario u)
-{
-    this.usuarios.put(u.getNombre(),u);
-}
+
+    public void añadirUsuario(Usuario u) {
+        if (u == null) {
+            logger.error("l'usuari està buit");
+
+        }
+        Usuario t = usuarios.put(u.getNombre(), u);
+        if (t != null) {
+            logger.error("l'usuari ja existeix");
+
+        } else {
+            logger.info("usuari afegit");
+
+        }
+    }
+
     public ArrayList<Producto> productosOrdenados() {
+        if(productosenventa == null)
+        {
+            logger.error("no hi han objectes disponibles");
+            return null;
+        }
         ArrayList<Producto> ordenados = new ArrayList<Producto>(productosenventa.values());
         Collections.sort(ordenados, new Comparator<Producto>() {
             public int compare(Producto product1, Producto product2) {
@@ -39,22 +59,22 @@ public void añadirUsuario(Usuario u)
             }
         });
         for (int x = 0; x < ordenados.size(); x++) {
-            System.out.println(ordenados.get(x).getNombre());
-            System.out.println(ordenados.get(x).getPrecio());
+            logger.info(ordenados.get(x).getNombre());
+            logger.info(ordenados.get(x).getPrecio());
         }
 
         return ordenados;
     }
 
-
-    //@Override
     public int crearPedido(Usuario usuari) {//una comanda pot estar buida s'omplirà mès endavant
+        logger.info("numero actual de comandes = "+idsdepedido);
         Pedido nuevo = new Pedido(this.idsdepedido,usuari);
         int actual = this.idsdepedido;
         this.idsdepedido++;
         this.todosLosPedidosEver.put(nuevo.id,nuevo);
         listaDePedidos.push(nuevo);
         //usuari.getMispedidos().add(nuevo);
+        logger.info("numero actual de comandes = "+idsdepedido);
         return actual;
         }
 
@@ -63,25 +83,16 @@ public void añadirUsuario(Usuario u)
         Pedido mipedido;
         mipedido = this.todosLosPedidosEver.get(id);//recupera el pedido
         mipedido.nuevoProducto(producto);
-
-       /* if (productosVendidos.containsKey(producto.getNombre())){
-            Integer numIncrementado = productosVendidos.get(producto.getNombre()) + 1;
-            //productosVendidos.put(producto.getNombre(), numIncrementado);
-            System.out.println("producto añadido por duplicado");
-        }else{
-            productosVendidos.put(producto.getNombre(), new Integer(1));
-            System.out.println("producto añadido");
-        }
-        */
     }
     @Override
     public boolean añadirProducto(Producto nuevo)
     {
         if (productosenventa.containsKey(nuevo.getNombre())){
-            //contiene ya el producto
+            logger.error("ja hi ha ningun article amb aquest nom a la venta");
             return false;
         }
         productosenventa.put(nuevo.getNombre(),nuevo);//añadimos producto
+        logger.info("el producte s'ha afegit a la venta");
         productosVendidos.put(nuevo.getNombre(),0);//inicialment ninguna venuda
         return true;
     }
@@ -91,7 +102,7 @@ public void añadirUsuario(Usuario u)
         for (Producto nuevo:milista)
         {
            int unidadesActuales = productosVendidos.get(nuevo.getNombre());
-          unidadesActuales  = unidadesActuales+1;
+           unidadesActuales  = unidadesActuales+1;
            productosVendidos.put(nuevo.getNombre(),unidadesActuales);
 
         }
@@ -104,20 +115,23 @@ public void añadirUsuario(Usuario u)
         Usuario usuari = nuevo.getUsuario();
         usuarios.get(usuari.getNombre()).mispedidos.add(nuevo);//afgim la comanda al propietari
         unidadesvendidas(nuevo);
-
         return nuevo;
     }
 
     @Override
     public ArrayList<Pedido> listarPedidos(Usuario u) {
       ArrayList<Pedido> mispedidos = u.getMispedidos();
+      if(u.getMispedidos()==null)
+      {
+          logger.info("no s'han processat encara cap comanda");
+      }
       ArrayList<Producto> productos = new ArrayList<Producto>();
       int numeroDelPedido;
         for (int x = 0; x < mispedidos.size(); x++) {
             productos = mispedidos.get(x).getProductos();
             numeroDelPedido=mispedidos.get(x).getId();
             for (Producto p:productos) {
-                System.out.println("numero de pedido" +numeroDelPedido+"nom del objecte venut"+p.getNombre());
+                logger.info("numero de comanda " +numeroDelPedido+" nom del objecte venut "+p.getNombre());
 
             }
 
@@ -132,7 +146,7 @@ public void añadirUsuario(Usuario u)
     public void productosDisponibles() {
         ArrayList<Producto> disponibles = new ArrayList<Producto>(productosenventa.values());
         for (int x = 0; x < disponibles.size(); x++) {
-            System.out.println(disponibles.get(x).getNombre());
+            logger.info(disponibles.get(x).getNombre());
         }
     }
 
@@ -140,11 +154,10 @@ public void añadirUsuario(Usuario u)
     @Override
     public void listarVentas()
     {
-
         Iterator it = productosVendidos.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry e = (Map.Entry)it.next();
-            System.out.println(e.getKey() + " " + e.getValue());
+            logger.info(e.getKey() + " " + e.getValue());
         }
 
 
--- marta/src/test/java/mitest.java
+++ marta/src/test/java/mitest.java
@@ -1,11 +1,14 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 
 public class mitest {
     ProducManagerImpl mimundo;
+    private static final Logger logger = LogManager.getLogger(mitest.class.getName());
     @Before
         public void setUp()
         {
@@ -16,6 +19,40 @@ public class mitest {
     {
         mimundo.getInstance().reiniciarSingleton();
     }
+
+    @Test
+    public void testProducteDuplicat()
+    { logger.info("iniciem test1");
+        Producto miProducto = new Producto("lampara", 20);
+        mimundo.añadirProducto(miProducto);
+        mimundo.añadirProducto(miProducto);
+    }
+    @Test
+    public void testUsuariRepetit(){
+        Usuario miusuari = new Usuario("marta");
+        mimundo.añadirUsuario(miusuari);
+        mimundo.añadirUsuario(miusuari);
+           
+    }
+    @Test
+    public void testRealitzarComanda(){
+        int pedidoactual;
+        Producto miProducto = new Producto("lampara", 20);
+        Producto miProducto2 = new Producto("lapiz", 50);
+        Producto miProducto3 = new Producto("casco", 35);
+        Usuario miusuari = new Usuario("marta");
+        mimundo.añadirProducto(miProducto);
+        mimundo.añadirProducto(miProducto2);
+        mimundo.añadirProducto(miProducto3);
+        mimundo.añadirUsuario(miusuari);
+        pedidoactual = mimundo.crearPedido(miusuari);
+        mimundo.añadirProductoaPedido(miProducto3,pedidoactual);
+        mimundo.añadirProductoaPedido(miProducto2,pedidoactual);
+        mimundo.añadirProductoaPedido(miProducto2,pedidoactual);//pedido actual = 1 casco 2 lapices
+        mimundo.listarPedidos(miusuari);//veiem les comandes que ens han processat
+        mimundo.servirPedido();
+        mimundo.listarPedidos(miusuari);
+    }
     @Test
     public void test()
 {   int pedidoactual;
@@ -25,30 +62,24 @@ public class mitest {
         Producto miProducto3 = new Producto("casco", 35);
         Usuario miusuari = new Usuario("marta");
         Usuario miusuari2 = new Usuario("anna");
-
         mimundo.añadirProducto(miProducto);
         mimundo.añadirProducto(miProducto2);
         mimundo.añadirProducto(miProducto3);
         mimundo.añadirUsuario(miusuari);
         mimundo.añadirUsuario(miusuari2);
-        //mimundo.productosOrdenados();
-        //mimundo.productosDisponibles();
         pedidoactual = mimundo.crearPedido(miusuari);
         mimundo.añadirProductoaPedido(miProducto3,pedidoactual);
         mimundo.añadirProductoaPedido(miProducto2,pedidoactual);
         mimundo.añadirProductoaPedido(miProducto2,pedidoactual);//pedido actual = 1 casco 2 lapices
-
         miotropedido = mimundo.crearPedido(miusuari2);
         mimundo.añadirProductoaPedido(miProducto,miotropedido);
         mimundo.añadirProductoaPedido(miProducto3,miotropedido);//2 lamparas
-        mimundo.listarPedidos(miusuari);
-        //System.out.println("vacio");
         mimundo.servirPedido();
-        System.out.println("primerpedido");
+        logger.info("Hem servit la primera comanda");
         mimundo.listarPedidos(miusuari);
         mimundo.listarVentas();
         mimundo.servirPedido();
-    System.out.println("segundopedido");
+        logger.info("Hem servit la segona comanda");
         mimundo.listarPedidos(miusuari2);
         mimundo.listarVentas();
 
